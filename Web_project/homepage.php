c043e8f5e61a81a810789ff540bb973b8d8d39b9
Adel <adelshatani.work@gmail.com>
2024-01-16 23:44:05 +0100

    Sto facendo delle prove con grid per provare a mettere in ordine i box delle locandine, ho solo aggiunto qualcosina sulla homepage (box container) e su style.css

--- Web_project/homepage.php
+++ Web_project/homepage.php
@@ -7,25 +7,40 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <!-- navigator messo nel file header.php -->
-        <?php require_once 'header.php'; ?>
-        <div class="banner">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-            <img src="https://w7.pngwing.com/pngs/186/294/png-transparent-banana-a-banana-food-banana-leaves-cartoon-thumbnail.png" alt="">
-
+    <!-- navigator messo nel file header.php -->
+    <?php require_once 'header.php'; ?>
+    <section class="box container">
+        <div class="post-box">
+            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
+            <h2 class="categoria">Prova</h2>
+            <a href="" class="post-title">Titolo di prova</a>
+            <span class="data">24/11/2001</span>
+            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
+                
         </div>
-
-        <div class="space"></div>
+        <div class="box">
+            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
+            <h2 class="categoria">Prova</h2>
+            <a href="" class="post-title">Titolo di prova</a>
+            <span class="data">24/11/2001</span>
+            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
+                
+        </div>
+            <div class="box">
+            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
+            <h2 class="categoria">Prova</h2>
+            <a href="" class="post-title">Titolo di prova</a>
+            <span class="data">24/11/2001</span>
+            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
+                
+        </div>
+        <div class="box">
+            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
+            <h2 class="categoria">Prova</h2>
+            <a href="" class="post-title">Titolo di prova</a>
+            <span class="data">24/11/2001</span>
+            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
+        </div>
+    </section>
 </body>
 </html>
