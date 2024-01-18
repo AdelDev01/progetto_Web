dcedd44c2362187fcd0cc5fcaaa99e795f2028c8
Adel <adelshatani.work@gmail.com>
2024-01-18 16:04:36 +0100

    prova col grid

--- Web_project/homepage.php
+++ Web_project/homepage.php
@@ -9,38 +9,18 @@
 <body>
     <!-- navigator messo nel file header.php -->
     <?php require_once 'header.php'; ?>
-    <section class="box container">
-        <div class="post-box">
-            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
-            <h2 class="categoria">Prova</h2>
-            <a href="" class="post-title">Titolo di prova</a>
-            <span class="data">24/11/2001</span>
-            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
-                
-        </div>
-        <div class="box">
-            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
-            <h2 class="categoria">Prova</h2>
-            <a href="" class="post-title">Titolo di prova</a>
-            <span class="data">24/11/2001</span>
-            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
-                
-        </div>
-            <div class="box">
-            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
-            <h2 class="categoria">Prova</h2>
-            <a href="" class="post-title">Titolo di prova</a>
-            <span class="data">24/11/2001</span>
-            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
-                
-        </div>
-        <div class="box">
-            <img src="https://nativalab.com/wp-content/uploads/2023/05/foto4-scaled.jpg" alt="" class="box-image">
-            <h2 class="categoria">Prova</h2>
-            <a href="" class="post-title">Titolo di prova</a>
-            <span class="data">24/11/2001</span>
-            <p class="descrizione">Testo di prova,blablalblablablalblbalblbalbalblabalblablabalbalblablabalblblalbalblalbalblalblblblblblblalbalbalbalablabl</p>
-        </div>
+
+    <section class="container">
+        <img src="img/image1.jpg" alt="">
+        <img src="img/image1.jpg" alt="">
+        <img src="img/image1.jpg" alt="">
+        <img src="img/image1.jpg" alt="">
+        <img src="img/image1.jpg" alt="">
+        <img src="img/image1.jpg" alt="">
     </section>
+
+
+
+
 </body>
 </html>
