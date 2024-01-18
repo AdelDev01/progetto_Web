06a62d2572094a07c7ff0918c1333621d31a7be3
Adel <adelshatani.work@gmail.com>
2024-01-18 16:51:42 +0100

    ho fatto le highlights

--- Web_project/homepage.php
+++ Web_project/homepage.php
@@ -7,17 +7,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
     <!-- navigator messo nel file header.php -->
     <?php require_once 'header.php'; ?>
+    </header>
 
-    <section class="container">
-        <img src="img/image1.jpg" alt="">
-        <img src="img/image1.jpg" alt="">
-        <img src="img/image1.jpg" alt="">
-        <img src="img/image1.jpg" alt="">
-        <img src="img/image1.jpg" alt="">
-        <img src="img/image1.jpg" alt="">
+    <section class="highlights">
+        <div class="highlight-images">
+            <img src="img/image1.jpg" alt="">
+            <img src="img/image1.jpg" alt="">
+            <img src="img/image1.jpg" alt="">
+            <img src="img/image1.jpg" alt="">
+            <img src="img/image1.jpg" alt="">
+
+            
+        </div>
+
+
     </section>
+
 
 
 
