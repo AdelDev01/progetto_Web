26276692c49d5a52ee70b6352d4054b05ba1e0f6
maurisac <mauriziosacc4gmail.com>
2024-01-19 18:03:06 +0100

    aggiunta gestione degli errori

--- Web_project/signup.php
+++ Web_project/signup.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="it">
-<!-- Questo è il codice che precedentemente era stato usato in login.html (solo la parte di registrazione)-->
 <head>
 
     <meta charset="UTF-8">
@@ -14,7 +13,7 @@
     <?php require_once 'header.php'?>
         <h1 id="titolo_registrazione">REGISTRAZIONE</h1>
         <p>Inserire email, username e password: </p>
-        <form action="file php per la registrazione" method="post">
+        <form action="./includes/signup.inc.php" method="post">
             <div id="form_registrazione">
                 <input type="email" name="email" placeholder="Inserisci la tua email"></input>
                 <br><br>
@@ -27,6 +26,35 @@
                 <button type="submit" name="submit">Registrati</button>
                 </div>
         </form>
+
+        <div id="messaggi_errori"> <!-- Gestione errori nella registrazione -->
+        <?php
+            if (isset($_GET['error'])) {
+                if($_GET['error'] == 'emptyinput'){
+                    echo '<p>Riempi tutti i campi!</p>';
+                }
+                if($_GET['error'] == 'invalidusername'){
+                    echo '<p>Username non valido! Inserisci solo caratteri alfanumerici!</p>';
+                }
+                if($_GET['error'] == 'invalidemail'){
+                    echo '<p>Email non valida! Inserisci solo caratteri alfanumerici!</p>';
+                }
+                if($_GET['error'] == 'passwordsdontmatch'){
+                    echo '<p>Le password non combaciano!</p>';
+                }
+                if($_GET['error'] == 'usernametaken'){
+                    echo '<p>Username o mail non disponibile!</p>';
+                }
+                if($_GET['error'] == 'stmtfailed'){
+                    echo '<p>Qualcosa è andato storto, prova di nuovo</p>';
+                }
+                if($_GET['error'] == 'none'){
+                    echo '<p>Registrazione effettuata con successo!</p>';
+                }
+                
+            }
+        ?>
+
 </body>
 
 </html>
