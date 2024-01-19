0c5cc4152adfb380c2136da717c3772291492539
Adel <adelshatani.work@gmail.com>
2024-01-19 16:00:21 +0100

    modifica ai box

--- Web_project/homepage.php
+++ Web_project/homepage.php
@@ -8,23 +8,103 @@
 </head>
 <body>
     <header>
+
     <!-- navigator messo nel file header.php -->
     <?php require_once 'header.php'; ?>
     </header>
-
     <section class="highlights">
         <div class="highlight-images">
-            <img src="img/image1.jpg" alt="">
-            <img src="img/image1.jpg" alt="">
-            <img src="img/image1.jpg" alt="">
-            <img src="img/image1.jpg" alt="">
-            <img src="img/image1.jpg" alt="">
 
-            
         </div>
+        <div class="highlight-images">
 
+        </div>
+        <div class="highlight-images">
 
+        </div>
+        <div class="highlight-images">
+
+        </div>
+        <div class="highlight-images">
+
+        </div>
+        <div class="highlight-images">
+
+        </div>
+        <div class="highlight-images">
+
+        </div>
+        <div class="highlight-images">
+
+        </div>
+        <div class="highlight-images">
+
+        </div>
     </section>
+    <div class="container">
+        <div class="box">
+            <div class="image">
+                <img src="img/image1.jpg" alt="" class="imag">
+
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image2.jpg" alt="" class="imag">
+                
+                </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image3.jpg" alt="" class="imag">
+                
+                </div>
+        </div>
+        <div class="box">
+            
+            <div class="image">
+                <img src="img/image4.jpg" alt="" class="imag">
+            </div>
+            <div class="info">
+                <h3>Nome film</h3>
+                <p>infoinfoinfoinfoinfoinfoinfoinfoninfoinfoinfoinfoinfoinfo</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image5.jpg" alt="" class="imag">
+                
+                </div>
+
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image6.jpg" alt="" class="imag">
+                
+            </div>
+        
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image7.jpg" alt="" class="imag">
+                
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image8.jpg" alt="" class="imag">
+                
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="img/image9.jpg" alt="" class="imag">
+
+            </div>
+                
+        </div>
+    </div>
 
 
 
