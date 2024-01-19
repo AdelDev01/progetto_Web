606b98e3ae6e632192d4ac6565610e2634cbdcc2
maurisac <mauriziosacc4gmail.com>
2024-01-19 15:56:02 +0100

    creato il footer.php

---
+++ Web_project/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <div class="footer-links">
+                <a href="http://www.github.com/AdelDev01/Progetto_Web">GitHub</a>
+                <a href="contatti.php">Contatti</a>
+            </div>
+            <div class="nome_sito">&copy; <?php echo date("Y"); ?> TOP</div>
+        </div>
+    </footer>
+</body>
+</html>
