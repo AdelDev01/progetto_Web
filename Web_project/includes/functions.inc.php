c052f5ddf5bb827e61356a2c95a46e987868642c
maurisac <mauriziosacc4gmail.com>
2024-01-19 18:09:30 +0100

    aggiunto file con tutte le funzioni attualmente utilizzate per poter collegarsi al db, sia per registrarsi che per loggarsi

---
+++ Web_project/includes/functions.inc.php
@@ -0,0 +1,167 @@
+<?php
+
+function emptyInputSignup($username, $email, $pwd, $pwdrepeat){
+    $result = null;
+    if (empty($username) || empty($email) || empty($pwd) || empty($pwdrepeat)){
+        $result = true;
+    }
+    else{  
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidStringInput($string){
+    $result = null;
+    if (!preg_match("/^[a-zA-Z0-9 '\"._,;:*+\\-<>]*$/", $string)){
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidEmail($email){
+    $result = null;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $result = true;
+    }
+    else{  
+        $result = false;
+    }
+    return $result;
+}
+
+function fieldMatch($firstField, $secondField){
+    $result = null;
+    if ($firstField !== $secondField){
+        $result = true;
+    }
+    else{  
+        $result = false;
+    }
+    return $result;
+}
+
+function usernameExists($conn, $username, $email){
+    $sql = "SELECT * FROM utenti WHERE username = ? OR email = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../signup.php?error=stmtfailed");
+        exit(); 
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($resultData)){
+        mysqli_stmt_close($stmt);
+        return $row;
+    }
+    else{
+        $resultData = false;
+        mysqli_stmt_close($stmt);
+        return $resultData;
+    }
+
+}
+
+function createUser($conn, $username, $email, $pwd){
+    $sql = "INSERT INTO utenti(username, email, password) VALUES (?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../signup.php?error=stmtfailed");
+        exit(); 
+    }
+
+    $hashedPwd= password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../signup.php?error=none");
+    exit();
+}
+
+function emptyInputCheck($firstField, $secondField){
+    $result = null;
+    if (empty($firstField) || empty($secondField)){
+        $result = true;
+    }
+    else{  
+        $result = false;
+    }
+    return $result;
+}
+
+function loginUser($conn, $username, $pwd){
+    $userExists = usernameExists($conn, $username, $username);
+    //mettere di nuovo $username come 3 parametro garantisce che il controllo nel DB venga effettuato sia sulla mail che sull'username,
+    // in modo da poter effettuare l'accesso con uno o con l'altro
+    if ($userExists === false){
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $userExists['password'];
+    $checkPwd = password_verify($pwd, $pwdHashed);
+
+    if ($checkPwd === false){
+        header("location: ../login.php?error=incorrectpassword");
+        exit();
+    }
+    else if ($checkPwd === true){
+        session_start();
+        $_SESSION["uid"] = $userExists["uid"];
+        $_SESSION["username"] = $userExists["username"];
+        header("location: ../homepage.php");
+        exit();
+    }
+}
+
+function reLoginUser($conn, $username, $pwd){
+    $userExists = usernameExists($conn, $username, $username);
+    //mettere di nuovo $username come 3 parametro garantisce che il controllo nel DB venga effettuato sia sulla mail che sull'username,
+    // in modo da poter effettuare l'accesso con uno o con l'altro
+    if ($userExists === false){
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $userExists['password'];
+    
+    if ($pwdHashed === $pwd){
+        session_start();
+        $_SESSION["uid"] = $userExists["uid"];
+        $_SESSION["username"] = $userExists["username"];
+        header("location: ../img/impostazioni.php?error=none");
+        exit(); 
+    }
+    else{
+        header("location: ../login.php?error=incorrectpassword");
+        exit();
+    }
+}
+
+// Funzione per ottenere le informazioni dal database
+
+function getUserInfo($conn, $username) {
+    // Esegui la query per ottenere le informazioni del campo dal database
+    $sql = "SELECT UID, username, password, email, data_creazione_acc FROM utenti WHERE username = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../homepage.php?error=stmtfailed");
+        exit(); 
+    }
+    mysqli_stmt_bind_param($stmt, "s", $username); 
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    // Ottieni il risultato della query
+    if ($row = mysqli_fetch_assoc($result)) {
+        return $row;
+    } else {
+        return null;
+    }
+}
