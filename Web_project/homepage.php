fe54e2ca29055fc2c4935e49f76b90d54f07a079
Adel <adelshatani.work@gmail.com>
2024-01-20 19:15:35 +0100

    css

--- Web_project/homepage.php
+++ Web_project/homepage.php
@@ -11,31 +11,39 @@
 
     <section class="highlights">
         <div class="highlight-images">
-            <a href=""></a>
+            <a href=""><img src="img/image1.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image2.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image3.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image4.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image5.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image6.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image7.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image8.jpg" alt="" class="imag"></a>
 
         </div>
         <div class="highlight-images">
+            <a href=""><img src="img/image9.jpg" alt="" class="imag"></a>
 
         </div>
    </section>
